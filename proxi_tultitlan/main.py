821979cd654b7bf14f5c32b94bd9b6243fe06e80
TuNombreDeGitHub <tuemail@github.com>
2025-12-02 13:33:00 -0600

    Actualización tultitlan

--- proxi_tultitlan/main.py
+++ proxi_tultitlan/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app)
 
 INVENTARIO_FILE = "inventarios_recibidos.json"
-JSON_AGENCIA = r"C:\Users\infob\Desktop\tultitlan\inventario_render.json"
+JSON_AGENCIA = r"C:\Users\infob\Desktop\tultitlan\Archivos\Export\inventario_render.json"
 
 def cargar_inventario():
     if not os.path.exists(INVENTARIO_FILE):
